04823c2038c8adf3de2650d5e3def4e43f37014d
Muteshi <lumteshi@gmail.com>
2021-01-08 19:27:05 +0300

    Setup the project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <Fragment>
+        <h2>Most popular repositories</h2>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
